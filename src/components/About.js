964d688e5c0fb6551447c20ae75cefbe840002b7
Cristian Cristea <cristiancristea.1987@gmail.com>
2017-07-28 14:37:32 +0200

    Update About component to render the content from Wordpress API

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React                from 'react';
+import PropTypes            from 'prop-types';
+import Navigation           from '../components/Navigation';
+import SmallHeader          from '../components/SmallHeader';
+import SocialIconsNavbar    from '../components/SocialIconsNavbar';
+import Footer               from '../components/Footer';
 
-const About = () => {
+const About = ({title, content, imageUrl, year}) => {
   return (
-    <h1>About Us</h1>
+    <div>
+      <Navigation />
+      <SmallHeader imageUrl={imageUrl} positionY={50} title={title}/>
+      <SocialIconsNavbar socialLinks={{facebook: 'http://facebook.com', instagram: 'http://instagram.com', mail: 'contact@aroundthebigworld.com'}} />
+      <div dangerouslySetInnerHTML={{__html: content}}></div>
+      <Footer year={year} />
+    </div>
   );
+}
+
+About.propTypes = {
+  title:    PropTypes.string.isRequired,
+  content:  PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  year:     PropTypes.number.isRequired
 }
 
 export default About;
