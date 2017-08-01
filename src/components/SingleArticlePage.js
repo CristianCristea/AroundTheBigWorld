edcc529817dfe8be275315bd3be107c206f524aa
Cristian Cristea <cristiancristea.1987@gmail.com>
2017-08-01 12:23:22 +0200

    Renamed SingleArticlePage to SinglePostPage

--- src/components/SingleArticlePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// TODO: create single article page component
-const SingleArticlePage = () => {
-    return (
-      <div>single article page</div>
-    );
-}
-
-export default SingleArticlePage;
