a46440ceeaab14b698e3ee0a8c3ac388ed9e09a1
Cristian Cristea <cristiancristea.1987@gmail.com>
2017-08-01 12:27:31 +0200

    Refactor getData function, update Routes - Navigation Component changed, add TODOs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { Component }             from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 // import sanitizeHTML from 'sanitize-html-react'; TODO: sanitize html input to except script tags
-import Layout from './components/Layout';
-import Homepage from './components/Homepage';
-import About from './components/About';
-// import Photos from './components/Photos';
-// import PageNotFound from './components/PageNotFound';
+import Layout                           from './components/Layout';
+import Homepage                         from './components/Homepage';
+import About                            from './components/About';
+import Photos                           from './components/Photos';
+import PageNotFound                     from './components/PageNotFound';
+import SinglePostPage                   from './components/SinglePostPage';
 
 /* routes
-  / - Homepage
-  /destinations/:destinationName - single destinations posts
+  /                                         - index
+  /destinations/:destinationName            - single destinations posts
   /destinations/:destinationsName/:postName - single post
-  /about
-  /disclaimer
-  /photos - link to instagram - evtl photos app
-  /404 - page not found
-  /search/:searchWords - TODO: serch page
+  /about                                    - about page
+  /disclaimer                               - disclaimer page
+  /photos - link to instagram               - evtl photos app
+  /404                                      - page not found
+  /search/:searchWords                      - TODO: serch page
 */
 
 export default class App extends Component {
@@ -24,8 +25,10 @@ export default class App extends Component {
 
     this.state = {
        posts: [],
-       pages: []
+       pages: [],
+       destinations: []
     };
+    this.capitalizeWord = this.capitalizeWord.bind(this);
   }
 
   // returns the current year
@@ -33,53 +36,74 @@ export default class App extends Component {
     return new Date().getFullYear();
   }
 
-  // fetch all posts from Wordpress API
-  getPosts(url) {
-    fetch(url)
-      .then((response) => response.json())
-      .then((response) => this.setState({posts: response}))
-      .catch(err => console.log(err))
-  }
-
-  // fetch all pages from Wordpress API
-  getPages(url) {
+  // fetch data from Wordpress API
+  getData(url, dataName) {
     fetch(url)
       .then(response => response.json())
-      .then(response => this.setState({pages: response}))
+      .then(response => this.setState({[dataName]: response}))
       .catch(err => console.log(err))
   }
 
   componentDidMount() {
-    this.getPosts('http://localhost/wordpress/wp-json/wp/v2/posts');
-    this.getPages('http://localhost/wordpress/wp-json/wp/v2/pages');
+    this.getData('http://localhost/wordpress/wp-json/wp/v2/posts', 'posts');
+    this.getData('http://localhost/wordpress/wp-json/wp/v2/pages', 'pages');
+    this.getData('http://localhost/wordpress/wp-json/wp/v2/categories', 'destinations');
   }
 
-  // filter the pages by name (Wordpress slug) returns the page with the resp. name
-  getPage(pages, name) {
-    const filterPages = pages.filter(page => {
-      return page.slug === name.toLowerCase();
-    });
-
-    return filterPages[0];
+  // filter an array of strings and return the first string that matches
+  filterFirstName(arr, name) {
+    return arr.filter(string => string.slug === name.toLowerCase())[0];
   }
 
-  // TODO implement selectLastPosts() to display on Homepage
+  // return an array of destinations, except all
+  getDestinations(destinations, destinationName) {
+    return destinations.reduce((destinations, destination) => {
+      if (destination.hasOwnProperty(destinationName) && destination[destinationName] !== 'all') {
+        destinations.push(destination[destinationName]);
+      }
+      return destinations;
+    }, [])
+  }
+
+  // capitalize a word
+  capitalizeWord(word) {
+    return word[0].toUpperCase() + word.slice(1).toLowerCase();
+  }
+
+  getCurrentPost(posts, postName) {
+    return posts.filter((post) => postName === post.slug)[0];
+  }
+
+  // TODO: implement selectLastPosts() to display on Homepage
 
   render() {
     let year = this.getCurrentYear();
-    let aboutPage = this.getPage(this.state.pages, 'about');
+    let aboutPage = this.filterFirstName(this.state.pages, 'about');
+    let destinations = this.getDestinations(this.state.destinations, 'slug');
 
     return (
+      // TODO: fix URL on refreshing and manually type
       <BrowserRouter>
         <Layout>
           <Switch>
-            <Route exact path="/" render={()=><Homepage articleThumbnails={this.state.posts} title="Around The Big World" year={year}/>} />
-            <Route path="/about" render={()=><About title={aboutPage.title.rendered} content={aboutPage.content.rendered} imageUrl={aboutPage.acf.header_image.url} year={year}/>} />
-            {/* <Route path="/photos" component={Photos} />
-            <Route path="/destinations/bali" render={ () => <AppDataContainer articleThumbnails={articleThumbnails.bali} /> } />
-            <Route path="/destinations/bangkok" render={ () => <AppDataContainer articleThumbnails={articleThumbnails.bangkok} /> } />
-            <Route path="/destinations/singapore" render={ () => <AppDataContainer articleThumbnails={articleThumbnails.singapore} /> } />
-            <Route component={PageNotFound} /> */}
+            <Route exact path="/" render={()=><Homepage destinations={destinations}
+                                                        posts={this.state.posts}
+                                                        title="Around The Big World"
+                                                        year={year}
+                                                        capitalizeWord={this.capitalizeWord}/>} />
+            <Route path="/about" render={()=><About title={aboutPage.title.rendered}
+                                                    content={aboutPage.content.rendered}
+                                                    imageUrl={aboutPage.acf.header_image.url}
+                                                    destinations={destinations}
+                                                    capitalizeWord={this.capitalizeWord}
+                                                    year={year}/>} />
+            <Route path="/destinations/:name" render={(props) => <SinglePostPage posts={this.state.posts}
+                                                                                 getCurrentPost={this.getCurrentPost}
+                                                                                 destinations={destinations}
+                                                                                 capitalizeWord={this.capitalizeWord}
+                                                                                 year={year} {...props}/>} />
+            <Route path="/photos" render={()=> window.location = "https://instagram.com"}/>
+            <Route component={PageNotFound} />
           </Switch>
         </Layout>
       </BrowserRouter>
